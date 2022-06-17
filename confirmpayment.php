2132e10693b7e652360568f8ce477ea989729f0e
sabina549 <kcsabina549@gmail.com>
2022-06-17 11:11:49 +0545

    skn

--- confirmpayment.php
+++ confirmpayment.php
@@ -5,18 +5,18 @@
     session_start();
     
     $msg="";
-    $id=$_GET['id'];
+    $booking_id=$_GET['id'];
     
-    $query= "SELECT * FROM `tripcost` WHERE booking_id='$id'";
+    $query= "SELECT * FROM `tripcost` WHERE booking_id='$booking_id'";
     $query1="UPDATE `tripcost` SET `id`='[value-1]',`booking_id`='[value-2]',`username`='[value-3]',
     `total_km`='[value-4]',`oil_cost`='[value-5]',`extra_cost`='[value-6]',`total_cost`='[value-7]',`paid`='[value-8]' 
     WHERE 1";
-    //echo $query;
+   // echo $query;
     $result= mysqli_query($connection,$query);
     $result1= mysqli_query($connection,$query1);
     $row= mysqli_fetch_assoc($result);
 
-    //echo $row['username'];
+   // echo $row['username'];
 ?>
 
 
@@ -51,11 +51,17 @@
           </div> 
             <div class="col-md-3"></div>
             <div class="col-md-6">
-                  <p><strong>Booking Id: </strong><?php  echo isset($row['booking_id']); ?></p>    
-                  <p><strong>Total Km: </strong><?php  echo isset( $row['total_km']); ?></p>    
-                  <p><strong>Oil Cost: </strong><?php isset ($row['oil_cost']); ?></p>    
-                  <p><strong>Extra Cost: </strong><?php isset ($row['extra_cost']); ?></p>    
-                  <p><strong>Total Cost: </strong><?php isset( $row['total_cost']); ?></p>
+                  <p><strong>Booking Id: </strong>
+                  <?php  echo isset($row['booking_id']); ?></p>    
+                  <p><strong>Total Km: </strong>
+                  <?php  echo isset( $row['total_km']); ?></p>    
+                  <p><strong>Oil Cost: </strong>
+                  <?php  echo isset ($row['oil_cost']); ?></p>    
+                  <p><strong>Extra Cost: </strong>
+                  <?php  echo isset ($row['extra_cost']); ?>
+                </p>    
+                  <p><strong>Total Cost: </strong>
+                  <?php echo isset( $row['total_cost']); ?></p>
                   
                 <form action="confirmpaymentaction.php?id=<?php echo $row['booking_id']; ?>" method="post">
                     <button class="btn btn-success">Confirm Payment</button>
--- confirmpaymentaction.php
+++ confirmpaymentaction.php
@@ -2,7 +2,7 @@
     $connection= mysqli_connect('localhost','root','','vehicle_management');
     session_start();
 
-    $id= $_GET['id'];
+    $booking_id= $_GET['id'];
 
     $sql="UPDATE `tripcost` SET `id`='[value-1]',`booking_id`='[value-2]',`username`='[value-3]',
     `total_km`='[value-4]',`oil_cost`='[value-5]',`extra_cost`='[value-6]',`total_cost`='[value-7]',`paid`='[value-8]' 
@@ -12,4 +12,6 @@
     if($result){
         header ('Location: bookinglist.php');
     }
+
+    echo  "Confirm Payment"
 ?>
